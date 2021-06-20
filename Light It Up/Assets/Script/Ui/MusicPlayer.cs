7fdada6f53603e746ad68bede4644f0c4e0baab7
GrimGenesis <grimgenesis96@gmail.com>
2021-06-20 11:10:59 +0530

    up

---
+++ Light It Up/Assets/Script/Ui/MusicPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioSource musicSource;
+    public AudioSource soundSource;
+    private float musicVolume = 1f;
+    private float soundVolume = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicSource.Play();
+        soundSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        musicSource.volume = musicVolume;
+        soundSource.volume = soundVolume;
+    }
+
+    public void updateMusic(float mvol)
+    {
+        musicVolume = mvol;
+    }
+
+    public void updateSound(float svol)
+    {
+        soundVolume = svol;
+    }
+
+}
